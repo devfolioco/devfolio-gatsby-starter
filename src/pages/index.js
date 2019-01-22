3f93db934020c72ff816cc414bed520e671e07c0
ajayns <ajay.ns@outlook.com>
2019-01-22 15:45:04 +0530

    :truck: Update project structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
+import Layout from '../components/common/Layout'
+import Image from '../components/common/Image'
+import SEO from '../components/common/SEO'
 
 const IndexPage = () => (
   <Layout>
