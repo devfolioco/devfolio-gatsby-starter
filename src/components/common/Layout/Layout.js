e965d9e797947739fcbbe5056ada3ebdd273b849
ajayns <ajay.ns@outlook.com>
2019-01-22 21:28:10 +0530

    :fire: Cleanup

--- src/components/common/Layout/Layout.js
+++ src/components/common/Layout/Layout.js
@@ -1,44 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StaticQuery, graphql } from 'gatsby';
 
 import Header from '../Header';
 
 import { Global } from './style';
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <Global />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+  <>
+    <Header siteTitle="Site Title" />
+    <Global />
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0px 1.0875rem 1.45rem`,
+        paddingTop: 0,
+      }}
+    >
+      {children}
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
+  </>
 );
 
 Layout.propTypes = {
