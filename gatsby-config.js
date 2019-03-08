a6626cf44a99f2816315122768884366f967073d
ajayns <ajay.ns@outlook.com>
2019-03-08 11:10:43 +0530

    :sparkles: Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: `static/favicon.svg`,
       },
     },
     {
