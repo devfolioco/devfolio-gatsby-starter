ab9d99c4ac5a5c09c7522c240b19f8fe0480a127
sai bhaskar <saibaaskar24091999@gmail.com>
2019-10-03 17:56:38 +0530

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
+import Navbar from '@common/Navbar';
 import Layout from '@common/Layout';
 import { Container } from '@components/global';
 
 const SecondPage = () => (
   <Layout>
+  <Navbar />
     <Container>
       <h1>Hi from the second page</h1>
       <p>Welcome to page 2</p>
