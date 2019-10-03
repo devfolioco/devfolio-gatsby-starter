a57fc7f2417c889a9466b09a7d7293d7bd41a9a1
Ojaswa Sharma <ojaswa1942@gmail.com>
2019-10-04 02:30:51 +0530

    add x.svg for mobile hamburger

--- src/components/common/Navbar/Navbar.js
+++ src/components/common/Navbar/Navbar.js
@@ -15,6 +15,7 @@ import {
 
 import { ReactComponent as DevfolioLogo } from '@images/devfolio-white.svg';
 import { ReactComponent as MenuIcon } from '@static/icons/menu.svg';
+import { ReactComponent as MenuCloseIcon } from '@static/icons/x.svg';
 
 const NAV_ITEMS = ['About', 'Gallery', 'Schedule', 'Sponsors', 'FAQ'];
 
@@ -65,7 +66,7 @@ class Navbar extends Component {
 
           <Mobile>
             <button onClick={this.toggleMobileMenu} style={{ color: 'white' }}>
-              <MenuIcon />
+              {(mobileMenuOpen)? <MenuCloseIcon /> : <MenuIcon /> }
             </button>
           </Mobile>
 
