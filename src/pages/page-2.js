785ef52a55b213ec4d8c68a8723357459811b5e0
sai bhaskar <saibaaskar24091999@gmail.com>
2019-10-04 11:44:44 +0530

    Update page-2.js
    adding content as module instead of hardcoded container

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
+import Contentp2 from '@sections/contentp2';
 import Layout from '@common/Layout';
 import { Container } from '@components/global';
 
 const SecondPage = () => (
   <Layout>
-    <Container>
-      <h1>Hi from the second page</h1>
-      <p>Welcome to page 2</p>
-      <Link to="/">Go back to the homepage</Link>
-    </Container>
+    </Contentp2>
   </Layout>
 );
 
