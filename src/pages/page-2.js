4895af7fdb60ddadbd3f6c682383aa7d09c30854
Jain-Sanchit <Jainsanchit157@gmail.com>
2020-10-01 20:05:04 +0530

    Added button to go back to home page on 2nd page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '@common/Layout';
-import { Container } from '@components/global';
+import { Container, Button } from '@components/global';
 
 const SecondPage = () => (
   <Layout>
     <Container>
       <h1>Hi from the second page</h1>
       <p>Welcome to page 2</p>
-      <Link to="/">Go back to the homepage</Link>
+      <Button as={Link} to="/" style={{ textDecoration: 'none' }}>
+        &larr; &nbsp; Go back to the homepage
+      </Button>
     </Container>
   </Layout>
 );
