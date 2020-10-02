16b0d2bf9e6c6a7922fee511534ac34e3b133467
Aadhitya A <59508546+echo-864@users.noreply.github.com>
2020-10-02 12:10:31 +0530

    Update packages via npm and modify page2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '@common/Layout';
+import Navbar from '@common/Navbar';
+import { Button } from '@components/global';
 import { Container } from '@components/global';
 
 const SecondPage = () => (
   <Layout>
+    <Navbar />
     <Container>
       <h1>Hi from the second page</h1>
       <p>Welcome to page 2</p>
-      <Link to="/">Go back to the homepage</Link>
+      <p>Feel free to go back to Homepage! :)</p>
+      <Button as={Link} to="/" style={{ textDecoration: 'none' }}>
+            Go to Homepage &nbsp; &larr;
+          </Button>
     </Container>
   </Layout>
 );
