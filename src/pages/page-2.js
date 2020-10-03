4823e2340b82a8104585e5a62d650194f2f46c1f
Prify Philip <amchu1714@gmail.com>
2020-10-03 11:16:54 +0530

    Page 2 : Idea Generation

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '@common/Layout';
-import { Container } from '@components/global';
+import Navbar from '@common/Navbar';
+
+import Header from '@sections/Header2';
+import Ideas from '@sections/Ideas';
+import Footer from '@sections/Footer';
+import { Container, Button } from '@components/global';
 
 const SecondPage = () => (
   <Layout>
     <Container>
-      <h1>Hi from the second page</h1>
-      <p>Welcome to page 2</p>
-      <Link to="/">Go back to the homepage</Link>
+      <Header />
+      <Ideas />
+      <Button as={Link} to="/" style={{ textDecoration: 'none' }}>
+            Go back to home page &nbsp; &rarr;
+      </Button>
+      <Footer />
     </Container>
   </Layout>
 );
